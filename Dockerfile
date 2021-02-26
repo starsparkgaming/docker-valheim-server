01e19fba63b05b9df3a9c2160d4de5f6a523f296
starsparkgaming <79425116+starsparkgaming@users.noreply.github.com>
2021-02-26 14:39:05 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM        ubuntu:latest
+
+RUN         dpkg --add-architecture i386 \
+            && apt update \
+            && apt upgrade -y \
+            && apt install -y tar curl lib32gcc1 libgcc1 ca-certificates \
+            && useradd -m -d /home/container container \
+
+USER        container
+ENV         HOME /home/container
+ENV NAME "Valheim Docker Server"
+ENV WORLD "Dedicated"
+ENV PASSWORD "12345"
+ENV PORT "2456"
+WORKDIR     /home/container
+
+COPY        ./entrypoint.sh /entrypoint.sh
+CMD         ["/bin/bash", "/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd /home/container
+
+## download and install steamcmd
+mkdir /home/container/steamcmd
+curl -SL -o steamcmd.tar.gz "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz"
+tar -xzvf steamcmd.tar.gz -C /home/container/steamcmd
+rm /home/container/steamcmd.tar.gz
+
+# install steamm app with steamcmd
+./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container/app +app_update 896660 +quit
+
+# run the Server
+eval /home/container/app/valheim_server.x86_64 -name "$NAME" -port $PORT -world "$WORLD" -password "$PASSWORD"
